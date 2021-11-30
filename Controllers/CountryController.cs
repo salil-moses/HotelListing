c55b2b1fa497b327c2545558e93f2d9432630254
salil-moses <dsis_contact@yahoo.co.in>
2021-11-30 15:38:27 +0530

    Added Controllers With Get Endpoints for Country and Hotel

---
+++ Controllers/CountryController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using HotelListing.IRepository;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<CountryController> _logger;
+        private readonly IMapper _mapper;
+
+        public CountryController(IUnitOfWork unitOfWork, ILogger<CountryController> logger, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetCountries()
+        {
+            try
+            {
+                // This gets countries with hotels
+                // var countries = await _unitOfWork.Countries.GetAll(null, null, new List<string> { "Hotels" });
+                var countries = await _unitOfWork.Countries.GetAll();
+                var results = _mapper.Map<IList<CountryDTO>>(countries); // map countries to DTO with automapper
+                // return Ok(countries);
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error fetching data for {nameof(GetCountries)}");
+                // throw;
+                return StatusCode(500, "Internal Server Error. Please Try Again Later");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetCountry(int id)
+        {
+            try
+            {
+                var country = await _unitOfWork.Countries.Get(q => q.Id == id, new List<string> { "Hotels" });
+                var result = _mapper.Map<CountryDTO>(country); // map countries to DTO with automapper
+                // return Ok(countries);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error fetching data for {nameof(GetCountry)}");
+                // throw;
+                return StatusCode(500, "Internal Server Error. Please Try Again Later");
+            }
+        }
+    }
+}
---
+++ Controllers/HotelController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using HotelListing.IRepository;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<HotelController> _logger;
+        private readonly IMapper _mapper;
+
+        public HotelController(IUnitOfWork unitOfWork, ILogger<HotelController> logger, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetHotels()
+        {
+            try
+            {
+                // var hotels = await _unitOfWork.Hotels.GetAll(null, null, new List<string> { "Country" });
+                var hotels = await _unitOfWork.Hotels.GetAll();
+                var results = _mapper.Map<IList<HotelDTO>>(hotels); // map countries to DTO with automapper             
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error fetching data for {nameof(GetHotels)}");
+                // throw;
+                return StatusCode(500, "Internal Server Error. Please Try Again Later");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetHotel(int id)
+        {
+            try
+            {
+                var hotel = await _unitOfWork.Hotels.Get(q => q.Id == id, new List<string> { "Country" });
+                var result = _mapper.Map<HotelDTO>(hotel); // map countries to DTO with automapper                
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error fetching data for {nameof(GetHotel)}");
+                // throw;
+                return StatusCode(500, "Internal Server Error. Please Try Again Later");
+            }
+        }
+    }
+}
